5b49f135592dbd97e5a5da0354fea8540c6fa2fc
Binoj D <dbinoj@gmail.com>
2015-04-07 19:12:04 +0530

    call function without shell=True

--- bench.py
+++ bench.py
@@ -1,5 +1,47 @@
 from joblib import Parallel, delayed
 from subprocess import call
-import multiprocessing
+import multiprocessing, csv, os
+from time import sleep
+from datetime import datetime
+
+SITES_CSV = "top-10.csv" # Fields (w/o headers): serial no, url/hostname
+REQUEST_COUNT = 5
+LOG_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+
+# Things which make my life easier
+num_cores = multiprocessing.cpu_count()
+time_now = lambda: datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+# Create log dir
+if not os.path.exists(LOG_FOLDER):
+    os.makedirs(LOG_FOLDER)
+
+r = csv.reader(open(SITES_CSV))
 
 
+# wget -p --no-cache -nd -nv --delete-after -e use_proxy=no -e http_proxy=172.16.0.2:8080 www.harvard.edu
+# --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0"
+
+def processInput(site):
+    for j in range(0, REQUEST_COUNT):
+        call(
+            [
+                "wget", 
+                "--no-cache", 
+                "-nd", 
+                "-nv", 
+                "--span-hosts", 
+                "-erobots=off", 
+                "--delete-after", 
+                "--user-agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0\"",
+                "-e use_proxy=yes", 
+                "-e http_proxy=172.16.0.2:8080", 
+                "--append-output=" + os.path.join(LOG_FOLDER, site),
+                site
+            ]
+        )
+    return True
+
+results = Parallel(n_jobs=num_cores)(delayed(processInput)(row[1]) for row in r)
+
+print results
