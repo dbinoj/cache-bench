f05ee8c348af25d64e35e4538e4ee425524765d5
Binoj D <dbinoj@gmail.com>
2015-04-08 22:43:07 +0530

    Try to contain leftover files created for reasons unknown.

--- bench.py
+++ bench.py
@@ -7,14 +7,20 @@ from datetime import datetime
 SITES_CSV = "top-1m.csv" # Fields (w/o headers): serial no, url/hostname
 REQUEST_COUNT = 1
 LOG_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+WORK_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "work")
+HOME_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 # Things which make my life easier
 num_cores = multiprocessing.cpu_count() * 25
 time_now = lambda: datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
 
-# Create log dir
+# Create log, work dir
 if not os.path.exists(LOG_FOLDER):
     os.makedirs(LOG_FOLDER)
+if not os.path.exists(WORK_FOLDER):
+    os.makedirs(WORK_FOLDER)
+
+os.chdir(WORK_FOLDER)
 
 r = csv.reader(open(SITES_CSV))
 
@@ -48,4 +54,5 @@ def processInput(site):
 
 results = Parallel(n_jobs=num_cores)(delayed(processInput)(row[1]) for row in r)
 
+os.chdir(HOME_FOLDER)
 print results
